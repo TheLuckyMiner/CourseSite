0b14dd47b5210d48564bae4a786910b1807ed4e3
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-03-28 11:26:25 +0300

    Bugfix

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ function displayHint(clicked_id){
     let id = clicked_id.slice(-1);
     let hint_id = '#hint' + id;
     let arrow_id = '#arrow' +  id;
+    if ($(hint_id).hasClass('hint__show')){
+        $(hint_id).fadeOut(400);
+    }
+    else {
+        $(hint_id).fadeIn(700);
+    }
     document.querySelector(arrow_id).classList.toggle('hint__show-arrow');
     document.querySelector(hint_id).classList.toggle('hint__show');
 
@@ -15,6 +21,13 @@ function displayAnswer(clicked_id){
     let id = clicked_id.slice(-1);
     let answer_id = "#anw" + id;
     let question_id = '#ask' + id;
-    document.querySelector(answer_id).classList.toggle('answer__show');
-    document.querySelector(question_id).classList.toggle('answer__show-arrow');
+    if($(answer_id).hasClass('answer__show')){
+        $(answer_id).fadeOut(400);
+    }
+    else {
+        $(answer_id).fadeIn(700);
+    }
+    document.querySelector(answer_id).classList.toggle('answer__show');  
+    document.querySelector(question_id).classList.toggle('answer__show-arrow'); 
+
 }
